11c408cc77c728d96c68225be903aad6261baa82
Akshat Singhal <akshat.singhal@tsgforce.com>
2024-04-14 01:17:48 +0530

    DOM Manipulation

---
+++ dom-manipulation/script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const inputEl = document.querySelector('.guess');
+
+const checkEl = document.querySelector('.check');
+const scoreEl = document.querySelector('.score');
+const numberEl = document.querySelector('.number');
+const highscoreEl = document.querySelector('.highscore');
+const messageEl = document.querySelector('.message');
+const randomNumber = Math.trunc(Math.random() * 20) + 1;
+let msg;
+checkEl.addEventListener('click', () => {
+  //Handle logic for scoring and checking here
+  const answer = Number(inputEl.value);
+  let currentScore = Number(scoreEl.textContent);
+  console.log(currentScore, typeof randomNumber, randomNumber, answer);
+
+  if (currentScore <= 1 && answer !== randomNumber) {
+    msg = '💥 You lost the game!';
+    currentScore = 0;
+  } else if (answer < randomNumber) {
+    currentScore--;
+    msg = 'Too Low !! 📉';
+  } else if (answer > randomNumber) {
+    currentScore--;
+    msg = 'Too high !! 📈';
+  } else {
+    if (Number(highscoreEl.textContent) < currentScore) {
+      highscoreEl.textContent = currentScore;
+    }
+    msg = '🎉🥇 Correct Number!';
+    numberEl.textContent = randomNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  }
+  scoreEl.textContent = currentScore;
+  messageEl.textContent = msg;
+});
---
+++ fundamental-1/index.js
@@ -0,0 +1,13 @@
+//var, let , const
+
+function abc() {
+  var a = "Akshat Singhal";
+  console.log(a);
+}
+
+abc();
+// console.log(a);
+
+//operators
+
+console.log(38 / 3);
