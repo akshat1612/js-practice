e5d115d8e60d33ccbc21b6eaa383c4da035f6f27
Akshat Singhal <akshat.singhal@tsgforce.com>
2024-04-14 01:18:16 +0530

    DOM Manipulation

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,37 +1,37 @@
-'use strict';
+"use strict";
 
-const inputEl = document.querySelector('.guess');
+const inputEl = document.querySelector(".guess");
 
-const checkEl = document.querySelector('.check');
-const scoreEl = document.querySelector('.score');
-const numberEl = document.querySelector('.number');
-const highscoreEl = document.querySelector('.highscore');
-const messageEl = document.querySelector('.message');
+const checkEl = document.querySelector(".check");
+const scoreEl = document.querySelector(".score");
+const numberEl = document.querySelector(".number");
+const highscoreEl = document.querySelector(".highscore");
+const messageEl = document.querySelector(".message");
 const randomNumber = Math.trunc(Math.random() * 20) + 1;
 let msg;
-checkEl.addEventListener('click', () => {
+checkEl.addEventListener("click", () => {
   //Handle logic for scoring and checking here
   const answer = Number(inputEl.value);
   let currentScore = Number(scoreEl.textContent);
   console.log(currentScore, typeof randomNumber, randomNumber, answer);
 
   if (currentScore <= 1 && answer !== randomNumber) {
-    msg = '💥 You lost the game!';
+    msg = "💥 You lost the game!";
     currentScore = 0;
   } else if (answer < randomNumber) {
     currentScore--;
-    msg = 'Too Low !! 📉';
+    msg = "Too Low !! 📉";
   } else if (answer > randomNumber) {
     currentScore--;
-    msg = 'Too high !! 📈';
+    msg = "Too high !! 📈";
   } else {
     if (Number(highscoreEl.textContent) < currentScore) {
       highscoreEl.textContent = currentScore;
     }
-    msg = '🎉🥇 Correct Number!';
+    msg = "🎉🥇 Correct Number!";
     numberEl.textContent = randomNumber;
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
   }
   scoreEl.textContent = currentScore;
   messageEl.textContent = msg;
